ade2ff1b2bd739eb115e4d4aaea5d0a5ae8d3249
Nicole Sosa <nls406@nyu.edu>
2022-10-08 18:09:34 -0400

    updating the main file with the dictionary,createRecipe function, and printAllRecipeNames function

--- Main.java
+++ Main.java
@@ -1,33 +1,86 @@
-
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.io.*;
 
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		// arrayList will store all the recipes
-		ArrayList <Recipe> recipeList = new ArrayList <> ();
+		// implementing a dictionary using HashMap 
+		// keys = recipe name 
+		// value = recipe object
+		Map <String, Recipe> recipes = new HashMap <String, Recipe>();
 		
-		// scanner will be used to recieve the user input
-		Scanner scan = new Scanner (System.in);
-		
-		// show the user their options in this program
-		displayMainWindow();
-		
-		// (1) user choose to add a recipe
-		if (scan.nextInt() == 1) {
-			addRecipe(recipeList,scan);
-			
-		}
-		
-			
-			
 	}
 	
+	
+	/* createRecipe information about parameters: */
+	
+	// fileName parameter description: 
+	// fileName is a String which is the name of your recipe txt file with its txt extension (i.e. pizza.txt)
+	// only if your recipe txt file is in the same directory of this program. Now if this recipe txt file is not in the same
+	// directory of this program, then fileName will be the full path of your recipe text file
+	// (this is from geeksforgeeks i.e.C:\\Users\\pankaj\\Desktop\\test.txt")
+	
+
+	// recipies parameter description:
+	// you just have to pass in our dictionary called recipes created in main
+	
+	public static void createRecipe(Map <String, Recipe> recipes,String fileName) throws FileNotFoundException {
+		
+		// using Scanner to read the text file 
+		File file = new File(fileName);
+		Scanner textFile = new Scanner(file);
+		
+		// read the text file information as Strings
+		String recipeName  = textFile.nextLine() ;
+		String strDescription = textFile.nextLine();
+		String strIngredients = textFile.nextLine();
+		String strInstruction = textFile.nextLine();
+	
+		// Since description is one big paragraph, we will get the individual sentences by using the split method
+		String [] description = strDescription.split("\\.");
+		
+		// Since ingredients is one big line where ingredients are seperated by commas, 
+		// we can get the individual ingredients using the split method
+		String [] ingredients = strIngredients.split(",");
+		
+		// Since instructions is one big paragraph, we will get the individual sentences by using the split method
+		String [] instructions = strInstruction.split("\\."); 
+		
+		// Now store the recipe information into a Recipe object
+		Recipe newRecipe = new Recipe(recipeName, description, ingredients, instructions);
+
+		// add this recipe object into our dictionary recipes
+		recipes.put(recipeName, newRecipe);
+		
+			
+		
+	}
+	
+	// recipies parameter description:
+	// you just have to pass in our dictionary called recipes in main
+	
+	public static void printAllRecipesNames(Map <String, Recipe> recipes) {
+		
+		System.out.println("\nBrowse All Recipes:");
+		
+		// print all the recipe names
+		for(String recipeName: recipes.keySet()){
+			
+		    System.out.println(" - "+recipeName);
+		}
+		
+		
+	}
+	
+	
+	
+	// I commented this out because I am not sure if you guys want to use it or not 
 	// displays the main UI the user interacts with 
+	/*
 	public static void displayMainWindow() {
 		System.out.println("\nRecipe Book:");
 		System.out.println("=============");
@@ -38,32 +91,8 @@ public class Main {
 		System.out.println("\n(4)Quit");
 		System.out.print("\nEnter Your Choice: ");
 	}
+	*/
 	
-	// allows user to add a recipe 
-	public static void addRecipe(ArrayList<Recipe> list, Scanner scan) {
-		
-		String workAround= scan.nextLine(); // Need this because nextInt messes things up for nextLine
-		
-		// ask user for the name of the recipe
-		System.out.print("Name of recipe: ");
-		String name = scan.nextLine();
-		
-		// ask user for the recipe description
-		System.out.print("Decription:");
-		String decription = scan.nextLine();
-		
-		// ask user for the ingredients
-		System.out.print("Ingredients:");
-		String ingredients = scan.nextLine();
-		
-		// ask user for instructions 
-		System.out.print("Instructions: ");
-		String instructions = scan.nextLine();
-		
-	
-		
-
-	}
 	
 
 
